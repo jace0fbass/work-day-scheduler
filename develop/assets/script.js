41bbcedc3bceb237174f4be2c8334c93dbc397b1
Jason <jajones414@gmail.com>
2022-09-30 22:30:37 -0500

    I have added the jquery to get the value of whatever is being entered and saved into an hour time block but still have to write how to store it.bI am also attempting to get the current date on the page.

--- develop/assets/script.js
+++ develop/assets/script.js
@@ -1,5 +1,55 @@
-$(document).ready(function() {
-    
-})
-var currentDay = $("#currentDay")
+
+ 
+
+// get current day
+var currentDay = $("currentDay").moment().format("MMM Do YY");
 console.log("currentDay")
+
+// Need function to save entered 
+
+
+
+
+
+//get saved info 
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"))
+$("#hour16 .description").val(localStorage.getItem("hours16"));
+$("hour17 .description").val(localStorage.getItem("hour17"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+// WHEN I refresh the page
+// THEN the saved events persist
