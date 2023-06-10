6d4b5ace8d76a766ea2386efab9d9afb9d659255
Jason <jajones414@gmail.com>
2023-06-10 13:11:11 -0500

    Minified css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -7,12 +9,20 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    plugins: [ new MiniCssExtractPlugin({
+        filename: 'styles.min.css'
+    })],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
             }
         ]
     },
+    optimization: {
+        minimizer: [
+           new CssMinimizerPlugin() 
+        ]
+    }
 }
